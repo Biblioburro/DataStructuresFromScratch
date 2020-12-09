0719b27c0eb264cd3649e6425a21ae1142ee8d80
Biblioburro <sammegginson@gmail.com>
2020-12-08 19:08:13 -0500

    expanded the driver to include a menu for the type of Data structure and developed the binary tree further

--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,40 @@
 import linkedLists.linkedList;
 import binaryTree.*;
+import java.util.Scanner;
 public class Driver {
- 
+	public static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
-	LinkedListTest();
 		
+	
+	int userChoice = 0;	
+	
+	//do while loop to handle user input
+	do {
+		printMenu();
+		userChoice = scan.nextInt();
+		switch(userChoice) {
+		case 1:
+			linkedListTest();
+			break;
+		case 2:
+			binaryTreeTest();
+			break;
+		default:
+		}
 		
+	}while(userChoice >0);
+	System.out.println("Program exited successfully");
+	scan.close();
 	}
-	public static void LinkedListTest() {
-		System.out.println("Initializing a linked list with random integers \nExpected Size : 10");
+	//method to prin the menu options
+	public static void printMenu() {
+		System.out.println("Enter the corresponding number\nfor the option you would like\n(enter a negative value to exit)");
+		System.out.println("1: linked list test");
+		System.out.println("2: binary tree testing\n");
+	}
+	//method to handle linked list testing
+	public static void linkedListTest() {
+		System.out.println("Initializing a linked list with random integers(long) \nExpected Size : 10");
 		//Declare and intialize a linkedList of integer types
 		linkedList<Long> L1 = new linkedList<Long>();
 		//populate the linked list with integers
@@ -18,7 +44,12 @@ public class Driver {
 		L1.printList();
 		System.out.print("Actual Size of linked List: ");
 		System.out.println(L1.size());
+		System.out.println();
 	}
-	
+	//method to handle binary tree testing
+	public static void binaryTreeTest() {
+		System.out.println("binarytreeTest");
+		System.out.println();
+	}
 	
 }
